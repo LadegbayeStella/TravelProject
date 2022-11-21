67192cb8237ba7cd43df7df4b789b990485932a2
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-11-21 15:24:39 +0100

    adding aothe swiper.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+// second slider
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
