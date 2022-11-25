843b8976b030e1017f7b6990034b5e97b426b976
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-11-25 19:04:33 +0100

    facing some bug here

--- js/script.js
+++ js/script.js
@@ -37,26 +37,38 @@ var swiper = new Swiper(".mySlider", {
       },
 });
 
-// var swiper = new Swiper(". mySlider", {
-//   loop:true, 
-//   grabCursor:true,
-//   spaceBetween: 20,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   breakpoints: {
-//       0: {
-//         slidesPerView: 1,
-//       },
-//       640: {
-//         slidesPerView: 2,
-//       },
-//       768: {
-//         slidesPerView: 3,
-//       },
-//       1024: {
-//         slidesPerView: 4,
-//       },
-//   },
-// });
+var swiper = new Swiper(".mySwiper", {
+  loop:true, 
+  grabCursor:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
+var swiper = new Swiper(".client-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          640: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 3,
+          },
+          1024: {
+            slidesPerView: 4,
+          },
+      },
+});
