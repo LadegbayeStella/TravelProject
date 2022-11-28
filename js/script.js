2b30075b3b24aeb853a660cd9f4265eb3f5b3107
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-11-28 14:08:33 +0100

    starting my footer

--- js/script.js
+++ js/script.js
@@ -72,3 +72,24 @@ var swiper = new Swiper(".client-slider", {
           },
       },
 });
+
+
+var swiper = new Swiper(".clients-slider", {
+  loop:true, 
+  grabCursor:true,
+  spaceBetween: 20,
+  breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1024: {
+        slidesPerView: 4,
+      },
+  },
+});
