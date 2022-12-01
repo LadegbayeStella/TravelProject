dfc1e663ef0aa4380cd02645427200eb5f8de3d0
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2022-12-01 03:23:55 +0100

    adding the navbar myHeader responsiveness

--- js/script.js
+++ js/script.js
@@ -93,3 +93,16 @@ var swiper = new Swiper(".clients-slider", {
       },
   },
 });
+
+let emmanuel = document.querySelector("#menu-btn");
+let myBoss = document.querySelector(".navbar");
+
+emmanuel.onclick = () => {
+    emmanuel.classList.toggle("fa-times");
+    myBoss.classList.toggle("active");
+}
+
+window.onscroll =()=> {
+    emmanuel.classList.remove("fa-times");
+    myBoss.classList.remove("active");
+}
